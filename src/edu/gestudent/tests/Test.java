28372454b9bc806840c22c8db34355bbfa36e8fd
megamoo456 <amin.bencheikh@esprit.tn>
2020-02-12 14:26:37 +0100

    Revert "Revert "Crud Beh(Done)""
    This reverts commit a9670e16b4f753c6e5d299131b4426020fe65ca8.

--- src/edu/gestudent/tests/Test.java
+++ src/edu/gestudent/tests/Test.java
@@ -5,8 +5,10 @@
  */
 package edu.gestudent.tests;
 
+import edu.gestudent.entities.Behaviour;
 import edu.gestudent.entities.user;
 import edu.gestudent.services.ServicesUsers;
+import edu.gestudent.services.behaviourCRUD;
 import java.sql.Date;
 import java.sql.SQLException;
 import java.util.logging.Level;
@@ -17,18 +19,31 @@ import java.util.logging.Logger;
  * @author Ayadi
  */
 public class Test {
-        public static void main(String[] args) throws SQLException {
-            ServicesUsers su =new ServicesUsers();
-             user u = new user("hasen","ayadi","dd.dd@esprit.tn","student","1995-05-06",22901120,"Tunisia","bellvue","female");
-            try {
-              su.ajouter(u);
-              // su.updateimage("171JMTB216","cssphoto");
-                //su.delete("171JMT212");
-            } catch (SQLException ex) {
-                Logger.getLogger(Test.class.getName()).log(Level.SEVERE, null, ex);
-            }
-           System.out.println(su.readAll());
 
-        }    
-    
+    public static void main(String[] args) {
+        behaviourCRUD be = new behaviourCRUD();
+//            ServicesUsers su =new ServicesUsers();
+//             user u = new user("gh","ayadi","dd.dd@esprit.tn","student","1995-05-06",22901120,"Tunisia","bellvue","female");
+        Behaviour b = new Behaviour(1, 2);
+
+      
+        
+        try {
+            // be.ajouterbeh(b);
+            be.updateatt(5,3);
+            //be.delete(1);
+            // su.ajouter(u);
+            // su.updateimage("171JMTB216","cssphoto");
+            //su.delete("171JMT212");
+        } catch (SQLException ex) {
+            Logger.getLogger(Test.class.getName()).log(Level.SEVERE, null, ex);
+        }
+      
+   
+        
+
+        System.out.println(be.afficherBehaviour());
+
+    }
+
 }
